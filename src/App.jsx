cb3e134ab20ca248b96798acf3087e385ae46c90
ayushhkkkk3585 <ayushkhalate04@gmail.com>
2024-08-04 21:39:29 +0530

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { useState } from "react";
 import "./App.css";
 import Content from "./components/Content";
 import TeamSection from "./components/TeamSection";
-import LifeAtSpringdale from "./components/LifeAtSpringdale";
+
 import Gallery from "./components/Gallery";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 
 
 function App() {
-  const [count, setCount] = useState(0);
+ 
 
   return (
     <>
